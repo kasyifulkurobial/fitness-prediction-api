039ce96330e7b78d0c4108574a1eb2ee5a315f1b
kaz-y30 <kurobikasyiful14@gmail.com>
2025-06-06 14:09:30 +0700

    push

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
+app.use(express.static('public')); // Serve static files from 'public' directory
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
